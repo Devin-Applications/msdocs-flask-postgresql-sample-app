2894bd8e4a037db1b8bd70fc47f6b9e001c971f4
Devin <devin@example.com>
2024-06-15 18:46:43 +0000

    Update production database configuration

--- azureproject/production.py
+++ azureproject/production.py
@@ -6,8 +6,8 @@ conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'],
-    dbname=conn_str_params['dbname']
+    dbuser='upsetlizard4',
+    dbpass='t9VffkAzIfCD5rLaF3UpmA',
+    dbhost='msdocs-postgres-server-68ecd9.postgres.database.azure.com',
+    dbname='flexibleserverdb'
 )
